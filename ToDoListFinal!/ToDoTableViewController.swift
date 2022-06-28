becfe0d52959a40edeec301392fbc400ba80ddf3
Eriifuji <erifujita29@icloud.com>
2022-06-28 13:35:21 -0300

    Finished Iteration 0

--- ToDoListFinal!/ToDoTableViewController.swift
+++ ToDoListFinal!/ToDoTableViewController.swift
@@ -18,9 +18,30 @@ class ToDoTableViewController: UIViewController {
         super.viewDidLoad()
         
         toDos = createToDos()
+        
+        
+        
+         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return toDos.count
+        }
+
+         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+          let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+          let toDo = toDos[indexPath.row]
+             
+             if toDo.important {
+               cell.textLabel?.text = "❗️" + toDo.name
+             } else {
+               cell.textLabel?.text = toDo.name
+             }
+
+          return cell
+        }
 
         // Do any additional setup after loading the view.
     }
+    
     
 
     
@@ -37,17 +58,7 @@ class ToDoTableViewController: UIViewController {
       return [swift, dog]
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return toDos.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-      let toDo = toDos[indexPath.row]
-
-      return cell
-    }
     
    
 
